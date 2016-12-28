592c6174cb3ba0c3caf5b38d9847a32ab65083e6
revanth <revanth@spinspire.com>
2016-12-28 12:44:33 -0500

    React forms

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+/* eslint-disable strict */
+$ = jQuery = require('jquery');
+var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+var Header = require('./common/headerPage');
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Header />
+        <div className="container-fluid">
+          <RouteHandler />
+        </div>
+      </div>
+    );
+
+  }
+
+});
+
+module.exports = App;
